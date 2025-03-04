e33c56d1ecfb7eb73e252145aef7448c4b6b889d
Gabi Pellense <gabipellense@gmail.com>
2025-03-03 21:38:46 -0300

    now
    aa

--- src/models/AWSModel.js
+++
@@ -1,8 +0,0 @@
-class imagemUsuario {
-    constructor(id,imagem) {
-        this.id = id;
-        this.imagem = imagem
-    }
-}
-
-module.exports = imagemUsuario;
--- src/models/usuario.js
+++
@@ -1,9 +0,0 @@
-class Usuario {
-    constructor(id, nome, data_criacao){
-        this.id = id,
-        this.nome = nome,
-        this.data_criacao = data_criacao
-    }
-}
-
-module.exports = Usuario;
